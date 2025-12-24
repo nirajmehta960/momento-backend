ffde5acf6c437db74af948b3e813b48c7d370294
Niraj Mehta <nirajmehta960@gmail.com>
2025-12-24 00:31:44 -0500

    Add indexes to Saves schema

--- Saves/schema.js
+++ Saves/schema.js
@@ -10,4 +10,9 @@ const saveSchema = new mongoose.Schema(
   { collection: "saves" }
 );
 
+// Indexes for performance optimization
+saveSchema.index({ user: 1 }); // For finding saved posts by user
+saveSchema.index({ post: 1 }); // For finding users who saved a post
+saveSchema.index({ user: 1, post: 1 }); // Compound index for checking save relationship
+
 export default saveSchema;
